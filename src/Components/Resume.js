42775605ee63f373aace2f78848dfd3a0ac859f7
dorelsaig <dorel@taz.co.il>
2022-05-11 11:57:22 +0300

    make it better

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -90,7 +90,7 @@ class Resume extends Component {
         <div className="row skill">
           <div className="three columns header-col">
             <h1>
-              <span>Favorite Tech</span>
+              <span>Skills</span>
             </h1>
           </div>
 
